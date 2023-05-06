172f6de191e1caa52caef657dd4bf15dcaf89508
vi xent <alagovincent03@gmailcom>
2023-05-06 14:25:53 -0700

    updated by vi

--- error-handling.js
+++ error-handling.js
@@ -1,14 +1,31 @@
 // 1.
 function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
 }
 
 let res = sum(null);
-console.log(res);
+try {
+    console.log(res())
+} catch (error) {
+    if (error instanceof TypeError) {
+        console.error(`wrong input: ${error.message}`)
+    } else {
+        console.log(error.message)
+    }
+}
+
+
+let sayName = (name) => {
+    if (typeof name !== String) {
+        throw new Error('give me a string')
+    } else {
+        console.log(name)
+    }
+}
 
 // 2.
 // tests
@@ -18,9 +35,17 @@ sayName(1);
 
 // 3.
 function greet(greeting) {
-  if (!greeting) {
-    throw new Error("There was no greeting given.");
-  }
+    if (!greeting) {
+        throw new Error("There was no greeting given.");
+    }
 
-  console.log(greeting);
+    console.log(greeting);
+}
+
+try {
+    console.log(greeting(undefined))
+} catch (error) {
+    console.error(error.message)
+} finally {
+    console.log('hello world')
 }
